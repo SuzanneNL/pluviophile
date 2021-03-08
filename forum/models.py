449d5a2731b680bab8daa342130c4bfaba7c8432
SuzanneNL <skr470@gmail.com>
2021-03-08 15:32:53 +0000

    Add date edited field for threads

--- forum/models.py
+++ forum/models.py
@@ -7,7 +7,8 @@ class Thread(models.Model):
     title = models.CharField(max_length=60)
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.CharField(max_length=2000)
-    date_created = models.DateTimeField(auto_now=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_edited = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title + ' by ' + str(self.creator)
