8a022dc0f354af41ee12a5b66d2003b92c26e333
SuzanneNL <skr470@gmail.com>
2021-03-12 17:56:02 +0000

    Create profile when a new user signs up, save profile when he changes it

--- pluviophile/urls.py
+++ pluviophile/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('forum/', include('forum.urls')),
+    path('profiles/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
