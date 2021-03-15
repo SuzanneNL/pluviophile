d749df1febea851f076b54f3f5bc793c9dfe732a
SuzanneNL <skr470@gmail.com>
2021-03-15 20:23:47 +0000

    Add basic styling to threads

--- forum/urls.py
+++ forum/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 from .views import (
     ForumView,
@@ -23,3 +25,6 @@ urlpatterns = [
     path('comment/delete/<int:pk>', DeleteCommentView.as_view(), name="delete_comment"),
     path('error', views.error, name='error')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
