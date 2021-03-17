9c01f9d8ac39d9f82e69db47b99354651e23fce6
SuzanneNL <skr470@gmail.com>
2021-03-17 19:18:36 +0000

    Start app 'blogs'

--- pluviophile/urls.py
+++ pluviophile/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('forum/', include('forum.urls')),
     path('profiles/', include('profiles.urls')),
+    path('blogs/', include('blogs.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
