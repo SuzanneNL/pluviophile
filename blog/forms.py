a8bb7bb81bbdf5ed02f77a93e9c2f01f613ce551
SuzanneNL <skr470@gmail.com>
2021-03-23 16:37:57 +0000

    Allow user to upload multiple images for a blog post

--- blog/forms.py
+++ blog/forms.py
@@ -5,4 +5,4 @@ from .models import BlogPost
 class BlogPostForm(forms.ModelForm):
     class Meta:
         model = BlogPost
-        fields = ('title', 'author', 'body', 'header_image')
+        fields = ('title', 'author', 'image_1', 'image_1_description', 'body_part_1', 'image_2', 'image_2_description', 'body_part_2', 'image_3', 'image_3_description', 'body_part_3')
