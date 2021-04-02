2c943b61f5a671a22f4c81973a95dc1054093a9d
SuzanneNL <skr470@gmail.com>
2021-04-02 21:00:11 +0000

    Update how information is displayed for admin

--- donation/admin.py
+++ donation/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import Donation
 
 
-admin.site.register(Donation)
+class DonationAdmin(admin.ModelAdmin):
+    readonly_fields = ('donation_number', 'date', 'amount', 'donor')
+
+    fields = ('donation_number', 'amount', 'donor', 'date')
+
+    ordering = ('-date',)
+
+
+admin.site.register(Donation, DonationAdmin)
