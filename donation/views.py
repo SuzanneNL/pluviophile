d9517af0b2fe78e51ca1aa03becb28a356933a22
SuzanneNL <skr470@gmail.com>
2021-04-04 20:13:06 +0000

    Add basic stripe payment functionality, with set amount

--- donation/views.py
+++ donation/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .forms import DonationForm
+from django.conf import settings
+import stripe
 
 
 def donate(request):
@@ -7,12 +9,26 @@ def donate(request):
 
 
 def charge(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=500,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     donation_form = DonationForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'donation/charge.html'
     context = {
         'donation_form': donation_form,
-        'stripe_public_key': 'test public key',
-        'client_secret': 'test client secret',
+        'stripe_public_key': 'stripe_public_key',
+        'client_secret': 'intent.client_secret',
     }
     return render(request, template, context)
 
