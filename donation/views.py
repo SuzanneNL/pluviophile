61135c8c45c01c8c88627163ea22496b77124b4c
SuzanneNL <skr470@gmail.com>
2021-04-04 18:58:40 +0000

    Add Stripe elements

--- donation/views.py
+++ donation/views.py
@@ -11,6 +11,8 @@ def charge(request):
     template = 'donation/charge.html'
     context = {
         'donation_form': donation_form,
+        'stripe_public_key': 'test public key',
+        'client_secret': 'test client secret',
     }
     return render(request, template, context)
 
