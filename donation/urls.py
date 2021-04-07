a134c9d4068364c173d8f078d7cffbb4b27bd971
SuzanneNL <skr470@gmail.com>
2021-04-07 16:20:44 +0000

    Add class method to webhook handler and webhook view

--- donation/urls.py
+++ donation/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 
 urlpatterns = [
     path('', views.donate, name='donate'),
     path('charge/', views.charge, name='charge'),
     path('donation_success/<donation_number>', views.donation_success, name='donation_success'),
+    path('wh/', webhook, name='webhook'),
 ]
