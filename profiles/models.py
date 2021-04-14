dead7aaca56771d6ff616578cb055d93e2843a24
SuzanneNL <skr470@gmail.com>
2021-04-14 16:37:21 +0000

    Set biography to optional (not required)

--- profiles/models.py
+++ profiles/models.py
@@ -10,7 +10,7 @@ from django_countries.fields import CountryField
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    biography = models.CharField(max_length=2000)
+    biography = models.CharField(max_length=2000, blank=True, null=True)
     country = CountryField(blank=True, null=True, blank_label='(Select your country)')
     date_of_birth = models.DateField(blank=True, null=True)
     member_since = models.DateTimeField(auto_now_add=True)
