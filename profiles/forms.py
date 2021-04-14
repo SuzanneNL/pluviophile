20d5fc87a15bd9a04e7d2d68e5cdfc69de41788f
SuzanneNL <skr470@gmail.com>
2021-04-14 20:11:06 +0000

    Disable date in the future for the date of birth on profiles

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Profile
+import datetime
 
 
 class DatePicker(forms.DateInput):
@@ -11,3 +12,9 @@ class ProfileForm(forms.ModelForm):
         model = Profile
         fields = ('date_of_birth', 'country', 'biography', 'avatar')
         widgets = {'date_of_birth': DatePicker}
+
+    def clean_date_of_birth(self):
+        date_of_birth = self.cleaned_data['date_of_birth']
+        if date_of_birth >= datetime.date.today():
+            raise forms.ValidationError("Select a date in the past")
+        return date_of_birth
