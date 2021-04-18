a619d27dc08defd8b3ca7f4912e06307ff04d5b8
SuzanneNL <skr470@gmail.com>
2021-04-18 20:09:13 +0000

    Fix bug: only check date of birth if it is filled in

--- profiles/forms.py
+++ profiles/forms.py
@@ -15,6 +15,7 @@ class ProfileForm(forms.ModelForm):
 
     def clean_date_of_birth(self):
         date_of_birth = self.cleaned_data['date_of_birth']
-        if date_of_birth >= datetime.date.today():
-            raise forms.ValidationError("Select a date in the past")
-        return date_of_birth
+        if date_of_birth:
+            if date_of_birth >= datetime.date.today():
+                raise forms.ValidationError("Select a date in the past")
+            return date_of_birth
