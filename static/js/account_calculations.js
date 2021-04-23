418cc805345b936ba696a6213b38cc50e9896335
SuzanneNL <skr470@gmail.com>
2021-04-23 23:08:39 +0000

    Move calculations on account page to separate js file

---
+++ static/js/account_calculations.js
@@ -0,0 +1,36 @@
+// The section below calculates the number of bookmarks.
+// If there are none, a 'no bookmarks' message is visible.
+let bookmarkscount = document.getElementsByClassName("bookmark").length
+    if(bookmarkscount == 0) {
+        showNoBookmarksMessage();
+    }
+    
+    function showNoBookmarksMessage() {
+    	document.querySelector('#no-bookmarks').classList.remove('hide');
+    }
+
+// The section below calculates the amount donated by all users.
+// And displays the amount on the account page.
+let donationCount = document.getElementsByClassName("donation-amount").length
+let donationAmount = donationCount * 5;
+
+if (donationCount > 1) {
+    showDonationAmount();
+}
+
+function showDonationAmount() {
+    document.getElementById("amount-donated").innerHTML = donationAmount;
+}
+
+// The section below calculates the amount donated by the logged in user,
+// And displays the amount on the account page.
+let yourDonationCount = document.getElementsByClassName("your-donation-amount").length
+let yourDonationAmount = yourDonationCount * 5;
+
+if (yourDonationCount > 1) {
+    showYourDonationAmount();
+}
+
+function showYourDonationAmount() {
+    document.getElementById("your-amount-donated").innerHTML = yourDonationAmount;
+}
