a041403b7d2d327a15ba1dc6d094525383cc131a
SuzanneNL <skr470@gmail.com>
2021-04-26 15:48:29 +0000

    Fix bug on account page, calculate user's donation amount first and always

--- static/js/account_calculations.js
+++ static/js/account_calculations.js
@@ -1,36 +1,32 @@
 // The section below calculates the number of bookmarks.
 // If there are none, a 'no bookmarks' message is visible.
-let bookmarkscount = document.getElementsByClassName("bookmark").length
-    if(bookmarkscount == 0) {
-        showNoBookmarksMessage();
-    }
-    
-    function showNoBookmarksMessage() {
-    	document.querySelector('#no-bookmarks').classList.remove('hide');
-    }
-
-// The section below calculates the amount donated by all users.
-// And displays the amount on the account page.
-let donationCount = document.getElementsByClassName("donation-amount").length
-let donationAmount = donationCount * 5;
-
-if (donationCount > 1) {
-    showDonationAmount();
+let bookmarkscount = document.getElementsByClassName("bookmark").length;
+if(bookmarkscount == 0) {
+	showNoBookmarksMessage();
 }
 
-function showDonationAmount() {
-    document.getElementById("amount-donated").innerHTML = donationAmount;
+function showNoBookmarksMessage() {
+	document.querySelector('#no-bookmarks').classList.remove('hide');
 }
 
 // The section below calculates the amount donated by the logged in user,
 // And displays the amount on the account page.
-let yourDonationCount = document.getElementsByClassName("your-donation-amount").length
+let yourDonationCount = document.getElementsByClassName("your-donation-amount").length;
 let yourDonationAmount = yourDonationCount * 5;
 
-if (yourDonationCount > 1) {
-    showYourDonationAmount();
+function showYourDonationAmount() {
+	document.getElementById("your-amount-donated").innerHTML = yourDonationAmount;
 }
 
-function showYourDonationAmount() {
-    document.getElementById("your-amount-donated").innerHTML = yourDonationAmount;
+showYourDonationAmount();
+
+// The section below calculates the amount donated by all users.
+// And displays the amount on the account page.
+let donationCount = document.getElementsByClassName("donation-amount").length;
+let donationAmount = donationCount * 5;
+
+function showDonationAmount() {
+	document.getElementById("amount-donated").innerHTML = donationAmount;
 }
+
+showDonationAmount();
