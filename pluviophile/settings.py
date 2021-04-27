06379abdaf8f802cce661d9fb9adbd76d7f47aaa
SuzanneNL <skr470@gmail.com>
2021-04-27 14:19:01 +0000

    Setup to send real e-mails

--- pluviophile/settings.py
+++ pluviophile/settings.py
@@ -113,8 +113,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'none'
@@ -219,3 +217,15 @@ DEFAULT_FROM_EMAIL = 'pluviophile@example.com'
 
 # Open weather map
 OPEN_WEATHER_MAP_KEY = os.environ.get('OPEN_WEATHER_MAP_KEY', '')
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'pluviophile@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
