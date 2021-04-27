fb3d8638f32c8faa62547cdf3f84112b99be2a23
SuzanneNL <skr470@gmail.com>
2021-04-27 23:44:16 +0000

    Check and correct python code in forum app

--- forum/urls.py
+++ forum/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
@@ -19,12 +18,17 @@ urlpatterns = [
     path('thread/<int:pk>', ThreadView.as_view(), name="thread"),
     path('start_thread/', StartThreadView.as_view(), name="start_thread"),
     path('thread/edit/<int:pk>', EditThreadView.as_view(), name="edit_thread"),
-    path('thread/delete/<int:pk>', DeleteThreadView.as_view(), name="delete_thread"),
-    path('thread/<int:pk>/add_comment/', AddCommentView.as_view(), name="add_comment"),
-    path('edit_comment/<int:pk>', EditCommentView.as_view(), name="edit_comment"),
-    path('comment/delete/<int:pk>', DeleteCommentView.as_view(), name="delete_comment"),
+    path('thread/delete/<int:pk>', DeleteThreadView.as_view(),
+         name="delete_thread"),
+    path('thread/<int:pk>/add_comment/', AddCommentView.as_view(),
+         name="add_comment"),
+    path('edit_comment/<int:pk>', EditCommentView.as_view(),
+         name="edit_comment"),
+    path('comment/delete/<int:pk>', DeleteCommentView.as_view(),
+         name="delete_comment"),
     path('error', views.error, name='error')
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
