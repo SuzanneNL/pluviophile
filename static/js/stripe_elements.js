927215addf31646792b3730ce1eb01c583440a79
SuzanneNL <skr470@gmail.com>
2021-04-28 20:13:52 +0000

    Disable form submission to test webhook

--- static/js/stripe_elements.js
+++ static/js/stripe_elements.js
@@ -78,7 +78,7 @@ form.addEventListener('submit', function(ev) {
 			$('#submit-button').attr('disabled', false);
 		} else {
 			if(result.paymentIntent.status === 'succeeded') {
-				form.submit();
+				// form.submit();
 			}
 		}
 	});
